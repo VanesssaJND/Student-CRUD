8bbe0727636b55fbfd3cc9b5743e010ac90206fa
Vanessa <104915551+VanesssaJND@users.noreply.github.com>
2024-04-02 13:15:02 -0500

    100% coverage StudentServiceTest

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,23 +1,22 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.StudentNotFoundException;
 import com.example.demo.student.exception.badRequestException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.time.LocalDate;
 import java.time.Month;
-
+import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
@@ -31,7 +30,7 @@ class StudentServiceTest {
         underTest = new StudentService(studentRepository);
     }
 
-
+    @DisplayName("Test the Get all students method")
     @Test
     void canGetAllStudents() {
         //when
@@ -39,7 +38,7 @@ class StudentServiceTest {
         //then
         verify(studentRepository).findAll();
     }
-
+    @DisplayName("Test if a new student can be add")
     @Test
     void canAddStudent() {
         //given
@@ -61,6 +60,7 @@ class StudentServiceTest {
         Student capturedStudent = studentArgumentCaptor.getValue();
         assertThat(capturedStudent).isEqualTo(student);
     }
+    @DisplayName("Test exception handling")
     @Test
     void willThrowWhenEmailIsTaken() {
         //given
@@ -80,9 +80,84 @@ class StudentServiceTest {
 
         verify(studentRepository, never()).save(any());
     }
+    @DisplayName("Test delete student method")
+    @Test
+    void canDeleteStudent() {
+        //given
+        Long studentId = 1L;
+        given(studentRepository.existsById(studentId))
+                .willReturn(true);
+        //When
+        underTest.deleteStudent(studentId);
+
+        //Then
+        verify(studentRepository).existsById(studentId);
+        verify(studentRepository).deleteById(studentId);
+    }
 
     @Test
-    @Disabled
-    void deleteStudent() {
+    void willThrowWhenNotFoundStudent() {
+        //given
+        Long studentId = 1L;
+        given(studentRepository.existsById(studentId))
+                .willReturn(false);
+
+        //when
+        //then
+        assertThatThrownBy(() -> underTest.deleteStudent(studentId))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Student with id " + studentId + " does not exist");
+        verify(studentRepository, never()).deleteById(studentId);
+    }
+
+    @DisplayName("JUnit test for updating student operation")
+    @Test
+    void canUpdateStudent() {
+        // Given
+        Long studentId = 1L;
+        String newName = "Elisa";
+        String newEmail = "lulu@gmail.com";
+        Student existingStudent = new Student(
+                "Vanessa",
+                "vane@gmail.com",
+                Gender.FEMALE,
+                LocalDate.of(2000, Month.APRIL, 17));
+
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(existingStudent));
+        when(studentRepository.findStudentByEmail(newEmail)).thenReturn(Optional.empty());
+
+        // When
+        underTest.updateStudent(studentId, newName, newEmail);
+
+        // Then
+        verify(studentRepository).findById(studentId);
+        verify(studentRepository).findStudentByEmail(newEmail);
+        assertThat(existingStudent.getName()).isEqualTo(newName);
+        assertThat(existingStudent.getEmail()).isEqualTo(newEmail);
+    }
+    @DisplayName("Throw exception if the email that is being try to update is taken")
+    @Test
+    void willThrowWhenEmailIsTakenWhileUpdating() {
+        //given
+        Long studentId = 1L;
+        String newName = "Elisa";
+        String newEmail = "lulu@gmail.com";
+
+        Student existingStudent = new Student(
+                "Vanessa",
+                "vane@gmail.com",
+                Gender.FEMALE,
+                LocalDate.of(2000, Month.APRIL, 17));
+
+        //when
+        given(studentRepository.findById(studentId))
+                .willReturn(Optional.of(existingStudent));
+        given(studentRepository.findStudentByEmail(newEmail))
+                .willReturn(Optional.of(new Student()));
+
+        //then
+        assertThatThrownBy(() ->underTest.updateStudent(studentId,newName,newEmail))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("email taken");
     }
 }
